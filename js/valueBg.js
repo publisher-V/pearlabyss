b51a3b823073c8aa3d6e48fcea5d1062f48cf82b
YouFive <84190472+publisher-V@users.noreply.github.com>
2022-09-30 12:11:55 +0900

    url update

--- js/valueBg.js
+++ js/valueBg.js
@@ -5,19 +5,19 @@ const wild = document.querySelector(".wild");
 const trust = document.querySelector(".trust");
 
 function Bg1() {
-  valueWrap.style.backgroundImage = "url(imgs/intro_value_persist_bg.jpg)";
+  valueWrap.style.backgroundImage = "url(/imgs/intro_value_persist_bg.jpg)";
 }
 
 function Bg2() {
-  valueWrap.style.backgroundImage = "url(imgs/intro_value_wild_bg.jpg)";
+  valueWrap.style.backgroundImage = "url(/imgs/intro_value_wild_bg.jpg)";
 }
 
 function Bg3() {
-  valueWrap.style.backgroundImage = "url(imgs/intro_value_trust_bg.jpg)";
+  valueWrap.style.backgroundImage = "url(/imgs/intro_value_trust_bg.jpg)";
 }
 
 function Bgreturn() {
-  valueWrap.style.backgroundImage = "url(imgs/value-home.png)";
+  valueWrap.style.backgroundImage = "url(/imgs/value-home.png)";
 }
 
 persist.addEventListener("mouseenter", Bg1);
